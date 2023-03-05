b5af085e5077602de757641951a4a158c5d3c2e7
Manasses <manasses.oliveira@dsbsolucoes.com.br>
2023-03-05 17:30:40 -0300

    first commit

---
+++ asp-net-core-agentcode-deploy/script/start_server.sh
@@ -0,0 +1,5 @@
+﻿export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion"] && \. "$NVM_DIR/bash_completion"
+cd /var/www/html/
+pm2 start "dotnet asp-net-core-agentcode-deploy.dll" --name tmro
