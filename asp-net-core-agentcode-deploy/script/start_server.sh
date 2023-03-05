742ca5eff1da90f5044ef5c8db28da0f60e2936a
Manasses <manasses.oliveira@dsbsolucoes.com.br>
2023-03-05 17:35:01 -0300

    first commit

--- asp-net-core-agentcode-deploy/script/start_server.sh
+++ asp-net-core-agentcode-deploy/script/start_server.sh
@@ -1,5 +1,5 @@
 ﻿export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion"] && \. "$NVM_DIR/bash_completion"
-cd /var/www/html/
+cd /var/www/html/agente-code-deploy/asp-net-core-agentcode-deploy/bin/Release/net6.0/asp-net-core-agentcode-deploy.dll
 pm2 start "dotnet asp-net-core-agentcode-deploy.dll" --name tmro
--- asp-net-core-agentcode-deploy/script/stop_server.sh
+++ asp-net-core-agentcode-deploy/script/stop_server.sh
@@ -1,1 +1,5 @@
-﻿
+﻿export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh"] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion"] && \. "$NVM_DIR/bash_completion"
+pm2 stop dotnet asp-net-core-agentcode-deploy
+pm2 delete asp-net-core-agentcode-deploy
